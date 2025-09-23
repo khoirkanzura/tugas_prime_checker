d4ed09ee6487a369039605990084f7b882cb4ef5
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2025-09-23 19:13:41 +0700

    first commit

---
+++ bin/tugas_prime_checker.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_prime_checker/tugas_prime_checker.dart' as tugas_prime_checker;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_prime_checker.calculate()}!');
+}
