e678cfabde9487ace617e3ed20d43061fc273262
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2025-09-23 19:25:20 +0700

    feat: tambah fitur input angka

--- bin/prime.dart
+++ bin/prime.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
 void main() {
   print("===Program Bilangan Prima===");
+
+  stdout.write("Masukkan angka: ");
+  int angka = int.parse(stdin.readLineSync()!);
+  print("Angka yang dimasukkan: $angka");
 }
